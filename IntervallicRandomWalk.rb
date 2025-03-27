41b81eecf2268a7d40bad5dd16fd72cb59f7f115
malcobe <malcobe@gmail.com>
2025-03-27 15:41:59 +0100

    The random walk is now played in a sequence
    The random walk is now played in a sequence, and it is now possible to use any intervals fitting the selected pitch range.

--- IntervallicRandomWalk.rb
+++ IntervallicRandomWalk.rb
@@ -1,61 +1,54 @@
 # Starting from a reference pitch, this script will start a random walk
 # by choosing among a collection of interval sizes.
-# The walk will take a number of steps before it goes back to the reference pitch.
-# Your goal is to keep track of the pitches visited, by recognizing
-# the intervals being used.
+# The walk will take a number of steps before a new walk is started by going back to the reference pitch.
+# Your goal is to keep track of the pitches being visited.
+# You can take a rest between consecutive walks.
 
 # Change the random seed to take a different walk
 use_random_seed 0
 
-# Set up the pitch range
+# Set the pitch range
 $min_pitch = :F3
 $max_pitch = :F4
 
-# Set up the reference pitch
+# Set the reference pitch
 # making sure it's within the pitch range above
 ref = :C4
 
-# Set up the interval sizes (in semitones) you'd like to practice on
-# making sure they fit within the pitch range above
-# starting from the central pitches of that range.
+# Set the interval sizes (in semitones) making sure they fit within the pitch range above.
 $int_sizes = [1,2]
 
-# Set up the steps of your walk
-# before you go back to your reference pitch.
-max_steps = 2
+# Set the steps of your walk
+# before another walk is played.
+max_steps = 3
+
+# Set the duration of each step in seconds
+step_duration = 1
+
+# Set the waiting time between consecutive walks in seconds
+waiting_time = 0
 
 # Now you can run the script
 
 $int_signs = [-1,1]
 
 def step(pitch)
+  size = $int_sizes.select{|s| pitch + s <= $max_pitch.to_i or pitch - s >= $min_pitch.to_i}.choose
   sign = $int_signs.choose
-  size = $int_sizes.choose
   if pitch+sign*size>$max_pitch.to_i or pitch+sign*size<$min_pitch.to_i
     sign = -1*sign
   end
   return pitch + sign*size
 end
 
-pitch1 = ref
-steps = 1
 loop do
-  pitch2 = step(pitch1)
-  2.times {
-    play pitch1
-    sleep 1
+  steps = [ref]
+  max_steps.times {
+    steps = steps.concat([step(steps[steps.length-1])])
   }
-  2.times {
-    play pitch2
-    sleep 1
-  }
-  play pitch1
-  sleep 4
-  if steps < max_steps
-    pitch1 = step(pitch1)
-    steps = steps + 1
-  else
-    pitch1 = ref
-    steps = 1
+  steps.each do |n|
+    play n
+    sleep step_duration
   end
+  sleep waiting_time
 end
